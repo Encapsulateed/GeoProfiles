952c4884fb3561bc6e1da9b9256d6fde0e2f816c
Encapsulateed <alexey.mitroshkin@gmail.com>
2025-05-22 19:44:02 +0300

    fix build

---
+++ GeoProfiles/Model/Generated/RefreshTokens.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeoProfiles.Model;
+
+public partial class RefreshTokens
+{
+    public Guid Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string Token { get; set; } = null!;
+
+    public DateTime ExpiresAt { get; set; }
+
+    public bool IsRevoked { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public virtual Users User { get; set; } = null!;
+}
