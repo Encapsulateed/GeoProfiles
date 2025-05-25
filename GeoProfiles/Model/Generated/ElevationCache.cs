4b2190ad75addeb39ff7c7774e2bd7dc7986c89d
Encapsulateed <alexey.mitroshkin@gmail.com>
2025-05-25 21:37:27 +0300

    run migration

---
+++ GeoProfiles/Model/Generated/ElevationCache.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using NetTopologySuite.Geometries;
+
+namespace GeoProfiles.Model;
+
+public partial class ElevationCache
+{
+    public Point Pt { get; set; } = null!;
+
+    public decimal ElevM { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
